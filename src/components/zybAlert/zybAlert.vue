50318052a1db46ab3bab3a5f64469df933a3fe18
zhangji02 <zhangji02@zuoyebang.com>
2018-09-05 20:40:32 +0800

    [update] finish alert

--- src/components/zybAlert/zybAlert.vue
+++ src/components/zybAlert/zybAlert.vue
@@ -4,7 +4,27 @@
     <p class="overall">概述</p>
     <p class="description">轻量级的信息反馈组件，在顶部居中显示，并自动消失。有多种不同的提示状态可选择。</p>
     <div class="content">
-      <el-button :plain="true" @click="open">点击显示普通提示</el-button>
+      <h2>默认样式</h2>
+      <div>
+        <el-button :plain="true" @click="open1('info')">普通提示</el-button>
+        <el-button :plain="true" @click="open1('warning')">警告提示</el-button>
+        <el-button :plain="true" @click="open1('success')">成功提示</el-button>
+        <el-button :plain="true" @click="open1('error')">失败提示</el-button>
+      </div>
+      <h2>暗黑样式</h2>
+      <div>
+        <el-button :plain="true" @click="open2('info')">普通提示</el-button>
+        <el-button :plain="true" @click="open2('warning')">警告提示</el-button>
+        <el-button :plain="true" @click="open2('success')">成功提示</el-button>
+        <el-button :plain="true" @click="open2('error')">失败提示</el-button>
+      </div>
+      <h2>纯白样式</h2>
+      <div>
+        <el-button :plain="true" @click="open3('info')">普通提示</el-button>
+        <el-button :plain="true" @click="open3('warning')">警告提示</el-button>
+        <el-button :plain="true" @click="open3('success')">成功提示</el-button>
+        <el-button :plain="true" @click="open3('error')">失败提示</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +36,27 @@ export default {
     return {};
   },
   methods: {
-    open() {
+    open1(type) {
       this.$message({
-        type: 'info',
-        duration: 0,
-        message: 'test'
+        type: type,
+        duration: 3000,
+        message: '这是一条消息提示',
+      });
+    },
+    open2(type) {
+      this.$message({
+        type: type,
+        duration: 3000,
+        message: '这是一条消息提示',
+        customClass: 'el-message-dark'
+      });
+    },
+    open3(type) {
+      this.$message({
+        type: type,
+        duration: 3000,
+        message: '这是一条消息提示',
+        customClass: 'el-message-light'
       });
     }
   }
@@ -30,6 +66,10 @@ export default {
 <style lang="less" scoped>
 .zyb-alert-page {
   padding: 40px;
+  h2 {
+    padding: 20px  0 20px 0;
+    font-size: 18px;
+  }
   .title {
     font-size:28px;
     font-family:PingFangSC-Regular;
@@ -55,14 +95,12 @@ export default {
   }
   .content {
     margin: 36px 0 0 0;
-    padding: 40px;
+    padding: 20px 40px 40px 40px;
     background: #FFF;
   }
 
 }
 </style>
-<style>
-.el-message {
-  min-width: 500px;
-}
+<style lang="less">
+  @import "./zybAlert.less";
 </style>
